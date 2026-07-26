cffeca9e139b7109437a331b53384580e75c128a
Sossi Grigoryan <sossi.grigoryan@gmail.com>
2026-07-26 19:54:38 +0400

    feat: add rate limiting and flood protection

--- src/bot/handlers/register-handlers.js
+++ src/bot/handlers/register-handlers.js
@@ -1,12 +1,15 @@
 import { registerStartHandler } from './start.handler.js';
 import { registerLanguageHandler } from './language.handler.js';
 import { registerWardrobeHandler } from './wardrobe.handler.js';
+import { rateLimitMiddleware } from '../../middlewares/rate-limit.middleware.js';
 
 /**
  * Registers all bot handlers.
  * @param {import('grammy').Bot} bot
  */
 export function registerHandlers(bot) {
+  bot.use(rateLimitMiddleware);
+
   registerStartHandler(bot);
   registerLanguageHandler(bot);
   registerWardrobeHandler(bot);
